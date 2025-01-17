2d6438b05b9c81da223bff194a0c73b206a9808f
Tom Marsh <ts.marsh@gmail.com>
2025-01-17 13:19:39 -0500

    tests fail in a much more attractive way

--- packages/meshql/src/index.ts
+++ packages/meshql/src/index.ts
@@ -1,11 +1,8 @@
 import {init} from "./server.js"
 const parser = require("@pushcorn/hocon-parser");
-import {Config} from "./configTypes.js"
 import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import {Application, Express} from "express";
 
-const argv: any = yargs(hideBin(process.argv))
+const argv: any = yargs(process.argv)
     .option('config', {
         type: 'string',
         description: 'Path to the config file ',
