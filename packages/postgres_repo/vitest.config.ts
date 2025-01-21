87bb1f636c4381cf75bf8a1d2567fd7e0c91f3fc
Tom Marsh <ts.marsh@gmail.com>
2025-01-21 16:38:09 -0500

    Giving github a few extra seconds to download things

--- packages/postgres_repo/vitest.config.ts
+++ packages/postgres_repo/vitest.config.ts
@@ -4,15 +4,16 @@ export default defineConfig({
     test: {
         globals: true,
         environment: "node",
+        testTimeout: 30000,
         coverage: {
             provider: "v8",
             reporter: ["text", "json", "html"],
             reportsDirectory: '../../coverage',
             exclude: [
-                '**/dist/**', // Exclude all dist directories
-                '**/node_modules/**', // Exclude node_modules
-                '**/test/**', // Optionally exclude test directories
-                '**/*.spec.ts', // Optionally exclude test files
+                '**/dist/**',
+                '**/node_modules/**',
+                '**/test/**',
+                '**/*.spec.ts',
             ],
         },
     },
