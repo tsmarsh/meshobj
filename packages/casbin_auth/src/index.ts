d52dd504efe833dd0dc6705de8c35e92b15ea757
Tom Marsh <ts.marsh@gmail.com>
2025-01-23 15:37:09 -0500

    going back to doing the check itself

--- packages/casbin_auth/src/index.ts
+++ packages/casbin_auth/src/index.ts
@@ -24,7 +24,15 @@ export class CasbinAuth implements Auth {
     }
 
     async isAuthorized(credentials: string[], data: Envelope): Promise<boolean> {
-        return await this.jwtAuth.isAuthorized(credentials, data)
+        const authorizedTokens = data.authorized_tokens;
+
+        // Allow access if authorized_tokens is empty or undefined
+        if (!authorizedTokens || authorizedTokens.length === 0) {
+            return true;
+        }
+
+        // Check if any of the credentials match authorized tokens
+        return authorizedTokens.some((token) => credentials.includes(token));
     }
 
 }
