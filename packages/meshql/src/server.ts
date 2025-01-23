d640a3cf2c99f616a1602d5b65cafdec898e76b7
Tom Marsh <ts.marsh@gmail.com>
2025-01-23 07:09:40 -0500

    I think the issue is with the way sqlite files are used?

--- packages/meshql/src/server.ts
+++ packages/meshql/src/server.ts
@@ -16,16 +16,10 @@ import {
     Searcher,
     Validator,
 } from "@meshql/common";
-import { MongoSearcher, MongoRepository } from "@meshql/mongo_repo";
-import { SQLiteSearcher } from "@meshql/sqlite_repo";
-import { open } from "sqlite";
-import sqlite3 from "sqlite3";
 import { Pool } from "pg";
-import { PostgresSearcher, PostgresRepository } from "@meshql/postgres_repo";
 import { Collection, MongoClient } from "mongodb";
 import { Crud } from "@meshql/restlette";
 import { JSONSchemaValidator } from "@meshql/restlette";
-import { SQLiteRepository } from "@meshql/sqlite_repo";
 import { JWTSubAuthorizer } from "@meshql/jwt_auth";
 import { Auth } from "@meshql/auth";
 import { CasbinAuth } from "@meshql/casbin_auth";
