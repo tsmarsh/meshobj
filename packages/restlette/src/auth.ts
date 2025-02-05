2e1b1dfd3bad01520a03d66266f6c743b4e846e0
Tom Marsh <ts.marsh@gmail.com>
2025-02-05 08:51:07 -0500

    Another unused file

--- packages/restlette/src/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export const authorizeResponse = (req: Request, res: Response): Response => {
-    if (req.headers.authorization !== undefined) {
-        res.header('Authorization', req.headers.authorization);
-    }
-
-    return res;
-};
