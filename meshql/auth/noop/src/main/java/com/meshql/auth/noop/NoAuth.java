d800550266ff8679526d8655e296db232f77206c
Tom Marsh <ts.marsh@gmail.com>
2025-02-27 16:53:32 -0500

    First cert test

--- meshql/auth/noop/src/main/java/com/meshql/auth/noop/NoAuth.java
+++ meshql/auth/noop/src/main/java/com/meshql/auth/noop/NoAuth.java
@@ -9,13 +9,26 @@ import java.util.List;
 import static com.tailoredshapes.underbar.ocho.UnderBar.list;
 
 public class NoAuth implements Auth {
+    List<String> tokens;
+    boolean authed;
+
+    public NoAuth() {
+        authed = true;
+        tokens = list("Token");
+    }
+
+    public NoAuth(List<String> tokens, boolean authed) {
+        this.tokens = tokens;
+        this.authed = authed;
+    }
+
     @Override
     public List<String> getAuthToken(Stash context) {
-        return list("Token");
+        return tokens;
     }
 
     @Override
     public boolean isAuthorized(List<String> credentials, Envelope data) {
-        return true;
+        return authed;
     }
 }
---
+++ meshql/repos/memory/src/test/java/com/meshql/repo/memory/MemoryRepositoryCertificationTest.java
@@ -0,0 +1,17 @@
+package com.meshql.repo.memory;
+
+import com.meshql.repos.certification.RepositoryCertification;
+import com.meshql.repositories.memory.InMemoryRepository;
+
+public class MemoryRepositoryCertificationTest extends RepositoryCertification {
+
+    @Override
+    public void init() {
+        this.repository = new InMemoryRepository();
+    }
+
+    @Override
+    public void tini() {
+
+    }
+}
