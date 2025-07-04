3964ab8b94812fa317f17bad0a5c35284b95c7a9
Tom Marsh <ts.marsh@gmail.com>
2025-07-04 07:38:57 -0400

    trying to be a little more aggressive with the hooktimeouts

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,9 @@
-import { defineWorkspace } from 'vitest/config';
+import { defineWorkspace, defineConfig } from 'vitest/config';
 
 export default defineWorkspace(['./packages/*/vitest.config.packages']);
+defineConfig({
+    test: {
+        hookTimeout: 120000, // this is what you’re missing
+        testTimeout: 120000, // optional, for consistency
+    },
+});
